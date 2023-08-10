82d15f573ebbed016502fd015b28b70d33204194
Carlos Ribeiro <ccadu86@gmail.com>
2023-08-10 11:18:56 -0300

    Utilização dos metodos MATCH e CASE

--- Algo.py
+++ Algo.py
@@ -3,23 +3,28 @@ from Classe import *
 def main():
     while True:
         try:
+
             menu = int(input("Qual valor deseja? > "))
 
-            if menu == 1:
+            match menu:
+                case 1:
+                        cir = Circulo(int(input("Informe o raio > ")))
+                        print(cir.calc_area())
+
+                case 2:
                     cir = Circulo(int(input("Informe o raio > ")))
-                    print(cir.calc_area())
+                    print(cir.calc_circo())
 
-            elif menu == 2:
-                cir = Circulo(int(input("Informe o raio > ")))
-                print(cir.calc_circo())
+                case 3:
+                    para = Paralelepipedo(int(input("Informe o Comprimento > ")), int(input("Informe a Largura > ")), int(input("Informe a Altura > ")))
+                    print(para.calc_volume())
 
-            elif menu == 3:
-                para = Paralelepipedo(int(input("Informe o Comprimento > ")), int(input("Informe a Largura > ")), int(input("Informe a Altura > ")))
-                print(para.calc_volume())
+                case 0:
+                    print("Saindo do software")
+                    break
 
-            elif menu == 0:
-                print("Saindo do software")
-                break
+                case _:
+                    print("Opção Invalida")
                     
         except Exception as erro:
             print("Valor invalido")
