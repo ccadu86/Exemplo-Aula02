b29c07887daee69f5f0811fb2c40fc748e8b1cbe
Carlos Ribeiro <ccadu86@gmail.com>
2023-08-17 14:08:48 -0300

    UPDATE

--- Algo.py
+++ Algo.py
@@ -1,24 +1,67 @@
 from Classe import *
 
 def main():
+
+    sistema = SistemaBancario()
+
     while True:
-        try:
-            menu = int(input("Qual valor deseja? > "))
-            match menu:
-                case 1:
-                    cir = Circulo(int(input("Informe o raio > ")))
-                    print(cir.calc_area())
-                case 2:
-                    cir = Circulo(int(input("Informe o raio > ")))
-                    print(cir.calc_circo())
-                case 3:
-                    para = Paralelepipedo(int(input("Informe o Comprimento > ")), int(input("Informe a Largura > ")), int(input("Informe a Altura > ")))
-                    print(para.calc_volume())
-                case 0:
-                    print("Saindo do software")
-                    break
-                case _:
-                    print("Opção Invalida")  
-        except Exception as erro:
-            print("Valor invalido")
-            print(erro.__class__.__name__)
+        print("---- Menu ----")
+        print("1. Cadastrar cliente")
+        print("2. Atualizar cadastro")
+        print("3. Excluir cliente")
+        print("4. Realizar depósito")
+        print("5. Realizar saque")
+        print("6. Realizar transferência")
+        print("7. Listar clientes cadastrados")
+        print("0. Sair")
+        print("----------------")
+        
+        opcao = input("Selecione uma opção: ")
+
+        if opcao == "1":
+            nome = input("Digite o nome do cliente: ")
+            cpf = input("Digite o CPF do cliente: ")
+            cliente = Cliente(nome, cpf)
+            sistema.cadastrar_cliente(cliente)
+        elif opcao == "2":
+            cpf = input("Digite o CPF do cliente a ser atualizado: ")
+            cliente = sistema.buscar_cliente(cpf)
+            if cliente:
+                novo_nome = input("Digite o novo nome (ou pressione Enter para manter o atual): ")
+                novo_cpf = input("Digite o novo CPF (ou pressione Enter para manter o atual): ")
+                cliente.atualizar_cadastro(novo_nome, novo_cpf)
+        elif opcao == "3":
+            cpf = input("Digite o CPF do cliente a ser excluído: ")
+            cliente = sistema.buscar_cliente(cpf)
+            if cliente:
+                cliente.excluir_cliente()
+        elif opcao == "4":
+            cpf = input("Digite o CPF do cliente: ")
+            cliente = sistema.buscar_cliente(cpf)
+            if cliente:
+                valor = float(input("Digite o valor do depósito: "))
+                cliente.deposito(valor)
+        elif opcao == "5":
+            cpf = input("Digite o CPF do cliente: ")
+            cliente = sistema.buscar_cliente(cpf)
+            if cliente:
+                valor = float(input("Digite o valor do saque: "))
+                cliente.saque(valor)
+        elif opcao == "6":
+            cpf_origem = input("Digite o CPF do cliente de origem: ")
+            cliente_origem = sistema.buscar_cliente(cpf_origem)
+            if cliente_origem:
+                cpf_destino = input("Digite o CPF do cliente de destino: ")
+                cliente_destino = sistema.buscar_cliente(cpf_destino)
+                if cliente_destino:
+                    valor = float(input("Digite o valor da transferência: "))
+                    cliente_origem.transferencia(cliente_destino, valor)
+        elif opcao == "7":
+            print("Clientes cadastrados:")
+            for cpf, cliente in sistema.clientes.items():
+                print(f"Nome: {cliente.nome}, CPF: {cliente.cpf}, Saldo: R${cliente.saldo:.2f}")
+        elif opcao == "0":
+            print("Saindo do sistema. Até logo!")
+            break
+        else:
+            print("Opção inválida. Por favor, selecione uma opção válida.")
--- Classe.py
+++ Classe.py
@@ -1,26 +1,51 @@
-class Circulo:
-    def __init__(self, raio):
-        self.raio = raio
+class Cliente:
+    def __init__(self, nome, cpf, saldo=0):
+        self.nome = nome
+        self.cpf = cpf
+        self.saldo = saldo
 
-    def calc_area(self):
-        area = 3.14 * self.raio ** 2
-        return area
+    def deposito(self, valor):
+        self.saldo += valor
+        print(f"Depósito de R${valor:.2f} realizado. Novo saldo: R${self.saldo:.2f}")
 
-    def calc_circo(self):
-        circo = 6.28 * self.raio
-        return circo
-    
-class Paralelepipedo:
-    def __init__(self, comprimento, largura, altura):
-        self.comprimento = comprimento
-        self.largura = largura
-        self.altura = altura
+    def saque(self, valor):
+        if self.saldo >= valor:
+            self.saldo -= valor
+            print(f"Saque de R${valor:.2f} realizado. Novo saldo: R${self.saldo:.2f}")
+        else:
+            print("Saldo insuficiente.")
 
-    def calc_volume(self):
-        volume = self.comprimento * self.largura * self.altura
-        return volume
+    def transferencia(self, destino, valor):
+        if self.saldo >= valor:
+            self.saldo -= valor
+            destino.deposito(valor)
+            print(f"Transferência de R${valor:.2f} realizada para {destino.nome}. Novo saldo: R${self.saldo:.2f}")
+        else:
+            print("Saldo insuficiente para a transferência.")
 
-class Cilindro:
-    def __init__(self, raio, altura):
-        self.raio = raio
-        self.altura = altura
+    def atualizar_cadastro(self, nome=None, cpf=None):
+        if nome:
+            self.nome = nome
+        if cpf:
+            self.cpf = cpf
+        print("Cadastro atualizado com sucesso.")
+
+    def excluir_cliente(self):
+        del self
+        print("Cliente excluído com sucesso.")
+
+
+class SistemaBancario:
+    def __init__(self):
+        self.clientes = {}
+
+    def cadastrar_cliente(self, cliente):
+        self.clientes[cliente.cpf] = cliente
+        print(f"Cliente {cliente.nome} cadastrado com sucesso.")
+
+    def buscar_cliente(self, cpf):
+        if cpf in self.clientes:
+            return self.clientes[cpf]
+        else:
+            print("Cliente não encontrado.")
+            return None
